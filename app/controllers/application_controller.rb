4081563bcd65f41e601cecbdf712927792d7d220
HenryStachowiak <stachowiak.h@northeastern.edu>
2021-08-04 15:46:05 -0400

    2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: 'Hello World!'
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    if session[:user_id]
+      # If we have current user we use it, otherwise we try to find it
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+  def logged_in?
+    # Turns current_user into a boolean
+    !!@current_user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 3)
+  end
+
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updates"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
@@ -21,6 +29,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up!"
+      session[:user_id] = @user.id
       redirect_to articles_path
     else
       render 'new'
@@ -31,6 +40,10 @@ class UsersController < ApplicationController
   private
   def user_params
     require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
